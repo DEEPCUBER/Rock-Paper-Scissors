d1083f7b997476b8267bd336767af1ae73ed36fc
AB_DEEPCODING <24200@my.sanctamaria.school.nz>
2026-03-07 20:28:22 +1300

    VERSIONS BACK IN MAIN FILE

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,7 +2,33 @@
 
 #Solo homework project (NO TEACHER HELP ALLOWED and CLANKERGPT/MICROSLOP HELP!)
 
-#Versions are available in Versions.md on the GITHUB page!
+
+# VIEW MY VERSION HISTORY HERE OF CREATING THIS PROGRAM!
+
+# ==== 5th March, 2026 Versions ==== #
+#Version 1: Finished up helper functions, main functions (In a skeleton), and constants 
+#Version 2: Finished setting up functions welcome text, and instructions. (Commited to github) 
+#Version 3: No code changes, just testing user input specifically on instructions 
+#Version 4: Finished game logic (Commited to github) 
+#Version 5: Completed the main loop that holds everything together 5/03/26 added 6/03/26
+# ==== 6th March, 2026 Versions ==== # 
+#Version 6: Added some code comments so people can understand what I'm trying to do 6/03/26
+#Version 7: Ran into a bug with player_score variable (FIXED)/(Commited to github) 6/03/26
+#Version 8: Attempting quality of life changes to the game to make it easier to play and pick up(clear_terminal) COMPLETED 6/03/26 (COMMITED TO GITHUB)
+#Version 9: Added a terminal response message before heading to instructions (forgot I deleted it) 6/03/26
+#Version 10: Teeny Tiny change line 72 added a space to input message 
+#Version 11: BIG UPDATE - Finally added quick moves for player to do (check line 36 for more info) and I have made the game easier to play (through clear text ofc) 
+# ==== 7th March, 2026 Versions ==== # 
+#Version 12: Fixed bug with variable name
+#Version 13: Added "loading" gimmick like some games and latency when loading each round!
+#Version 14: Continuing on version 13 with some stuff I missed
+#Version 15: Teeny tiny update
+#Version 16: Another teeny tiny update
+#Version 17: Attempt of making the move rock available with r (For rock etc) Les go I did it!
+#Version 18: Added a victory and losing animation
+#Version 19: Added a few comments to parts I haven't explained how they work yet and a few renames
+#Version 20: Added versions back since it looks weird on github
+
 
 #Modules - These help with stuff that is important to this game especially helpful stuff
 import random #Helps with randomizing the answers that the terminal clanker will choose form
